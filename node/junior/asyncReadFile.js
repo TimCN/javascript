9184f50fa1a31e07c678861fd48b7c8972322e2d
timcn <tim.shanghai@outlook.com>
2016-01-21 00:10:29 +0800

    be familiar with node async

--- node/junior/asyncReadFile.js
+++ node/junior/asyncReadFile.js
@@ -1,17 +1,45 @@
-var http = require('http');
+var http =require('http');
 var fs = require('fs');
+var qs = require('querystring');
+var _url = require('url');
 
-http.createServer( function (req, res) {
+var counter = 0;
 
-  fs.readFile('helloWorld.js', 'utf8', function (err, data) {
-    res.writeHead(200,{'content-type':'text/plain'});
-    if ( err ) {
-      res.write("Could not find or open file for reading\n");
-    } else {
-      res.write(data);
-    }
-    res.end();
-  })
+function writeNumbers (res) {
+  for(var i = 0; i < 100; i++ ){
+    counter++;
+    res.write(counter.toString() +"\n");
+  }
+}
+
+
+http.createServer(function(req, res) {
+  var query = _url.parse(req.url).query;
+  var app = "./files/" + qs.parse(query).q + ".txt";
+
+
+  // content header
+  res.writeHead(200,{'content-type':'text/plain'});
+
+  writeNumbers(res);
+
+
+  //timer to open file and read contents
+  setTimeout(function(){
+    fs.readFile(app, 'utf8', function (err, data) {
+      if(err){
+        res.write("Could not find or open file for reading \n");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    })
+  },200);
+
+
+
 }).listen(8124);
+
+
 
 console.log("server running at 8124");
